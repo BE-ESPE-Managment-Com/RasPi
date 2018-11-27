7b45fc3eae310953a697668d1b1ae2c64074f1d0
Tanguy Simon <tsimon@etud.insa-toulouse.fr>
2018-11-27 18:04:12 +0100

    Update CAN_appli.c

--- CAN_appli.c
+++ CAN_appli.c
@@ -1,21 +1,35 @@
-static void vCAN_Send_u64(uint32_t __MSG_ID, uint8_t __MSG_LENGTH, uint64_t __Data, uint32_t __Timeout)
+//envoie un message CAN
+static void vCAN_Send_u64(uint32_t __MSG_ID, uint8_t __MSG_LENGTH, uint64_t * __Data, uint32_t __Timeout)
 {
 	hcan.pTxMsg = &myTxMessage;
 	
 	myTxMessage.StdId = __MSG_ID;
 	myTxMessage.ExtId = __MSG_ID<<16;
 	myTxMessage.DLC = __MSG_LENGTH;
-	while 
-	myTxMessage.Data[0] = (uint8_t)(__Data>>56);
-	myTxMessage.Data[1] = (uint8_t)(__Data>>48);
-	myTxMessage.Data[2] = (uint8_t)(__Data>>40);
-	myTxMessage.Data[3] = (uint8_t)(__Data>>32);
-	myTxMessage.Data[4] = (uint8_t)(__Data>>24);
-	myTxMessage.Data[5] = (uint8_t)(__Data>>16);
-	myTxMessage.Data[6] = (uint8_t)(__Data>>8);
-	myTxMessage.Data[7] = (uint8_t)(__Data);
+	for(int i, i++, i<__MSG_LENGTH)
+	{
+		myTxMessage.Data[i] = (uint8_t)(__Data[i]);
+	}
 	myTxMessage.IDE = CAN_ID_EXT;
 	myTxMessage.RTR = CAN_RTR_DATA;
 	
 	HAL_CAN_Transmit_IT(&hcan);
+}
+
+
+// Configure un filtre matériel autorisant les messgaes CAN utiles.
+void vCAN_SetFilter(uint16_t __u16_CAN_ID, uint8_t __u8_FilterNum)
+{
+	myFilter.BankNumber = 1;
+	myFilter.FilterFIFOAssignment = CAN_FILTER_FIFO1;
+	myFilter.FilterIdHigh = __u16_CAN_ID;
+	myFilter.FilterIdLow = 0x0000;
+	myFilter.FilterMaskIdHigh = 0x0000;
+	myFilter.FilterMaskIdLow = 0x0000;
+	myFilter.FilterMode = CAN_FILTERMODE_IDMASK;
+	myFilter.FilterNumber = __u8_FilterNum;
+	myFilter.FilterScale = CAN_FILTERSCALE_32BIT;
+	
+	myFilter.FilterActivation = ENABLE;
+	HAL_CAN_ConfigFilter(&hcan, &myFilter);
 }
