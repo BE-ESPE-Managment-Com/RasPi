c4f6af89db0381a5cbb8f574d9cf983db114b6b3
Tanguy Simon <tsimon@etud.insa-toulouse.fr>
2018-11-28 16:52:00 +0100

    creation fonctions CAN appli

--- CAN_appli.py
+++ CAN_appli.py
@@ -3,7 +3,10 @@
 #Project : BE Interdisciplinaire ESPE
 
 ############################################################################
-
+import RPi.GPIO as GPIO
+import can
+import time
+import os
 
 
 
@@ -19,32 +22,74 @@
 
 
 ############################################################################
-class CAN_Message :
+def PICAN_LED_init() :
+	led = 22
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(led,GPIO.OUT)
+	GPIO.output(led,True)
+	return
+	
+def CAN_init() :
+	print('Bring up CAN0....')
+	# Bring up can0 interface at 500kbps
+	os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
+	time.sleep(0.1)
+	
+	try:
+		bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+	except OSError:
+		print('Cannot find PiCAN board.')
+		GPIO.output(led,False)
+		exit()
+	return
+
+def CAN_deinit() :
+	print('Bring down CAN0....')
+	os.system("sudo /sbin/ip link set can0 down")
+	return
+	
+class c_CAN_Message :
 	"""Structure of a CAN message"""
 	def __init__(self, _ID, _Length, _Data):
 		self.ID = _ID
 		self.Length = _Length
 		self.Data = _Data
-		
+
+def CAN_Send_msg(CAN_Msg) :
+	msg = can.Message(arbitration_id=CAN_Msg.ID,CAN_Msg.Data,extended_id=False)
+	bus.send(msg)
+	return
+	
+def CAN_Receive_msg() :
+	message = bus.recv()	# Wait until a message is received.
+	CAN_Msg = c_CAN_Message(message.arbitration_id, len(message.data), message.data) #creates object of class CAN_Message
+	return CAN_Msg
+	
+def CAN_RX_Parser(CAN_Msg):
+	if CAN_Msg.ID == BMS_MMS_SOC_ID :
+		Battery_SoC = CAN_Msg.Data[0]
+
+			
 #CAN IDs
-MPPT_MMS_STAT_ID = 	0x4211
-BMS_MMS_STAT =		0x4311
-INV_MMS_STAT =		0x4411
-LSW_MMS_STAT =		0x4511
-MPPT_MMS_PWR =		0x3212
-BMS_MMS_SOC =		0x3313
-BMS_MMS_PWR =		0x3312
-LSW_MMS_LDATA =	0x3514
-MMS_MPPT_EN =		0x1120
-MMS_BMS_EN =		0x1130
-MMS_INV_EN =		0x1140
-MMS_LSW_EN =		0x1150
-MMS_MPPT_MAXPWR =	0x2125
-MMS_BMS_SWINV =	0x1126
-MMS_LSW_SWLOADS =	0x2156
-BMS_MMS_OCH =		0x1317
-BMS_MMS_UCH =		0x1318
-BMS_MMS_OT =		0x1319
+MPPT_MMS_STAT_ID = 		0x4211
+BMS_MMS_STAT_ID =		0x4311
+INV_MMS_STAT_ID =		0x4411
+LSW_MMS_STAT_ID =		0x4511
+MPPT_MMS_PWR_ID =		0x3212
+BMS_MMS_SOC_ID =		0x3313
+BMS_MMS_PWR_ID =		0x3312
+LSW_MMS_LDATA_ID =		0x3514
+MMS_MPPT_EN_ID =		0x1120
+MMS_BMS_EN_ID =		0x1130
+MMS_INV_EN_ID =		0x1140
+MMS_LSW_EN_ID =		0x1150
+MMS_MPPT_MAXPWR_ID =	0x2125
+MMS_BMS_SWINV_ID =		0x1126
+MMS_LSW_SWLOADS_ID =	0x2156
+BMS_MMS_OCH_ID =		0x1317
+BMS_MMS_UCH_ID =		0x1318
+BMS_MMS_OT_ID =		0x1319
 
 #CAN message lenghts
 MPPT_MMS_STAT_LENGTH =		1
@@ -65,11 +110,4 @@ MMS_LSW_SWLOADS_LENGTH 		8
 BMS_MMS_OCH_LENGTH =		1
 BMS_MMS_UCH_LENGTH =		1
 BMS_MMS_OT_LENGTH =			1
-
-def CAN_RX_Parser(Msg):
-	if Msg.ID == MPPT_MMS_STAT :
-		
-	elif 
-
-
 
