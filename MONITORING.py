4007c9b18d75902a581dcdef2a7cdf51259dbfc6
Tanguy Simon <tsimon@etud.insa-toulouse.fr>
2019-01-15 11:11:41 +0100

    added comments

--- MONITORING.py
+++ MONITORING.py
@@ -1,7 +1,7 @@
 #File Name : MONITORING.py
 #Author : Tanguy SIMON
 #Project : BE Interdisciplinaire ESPE
-#Abstract : Algorithms for the monitoring function
+#Abstract : Algorithms for the monitoring function. All power is Apparent power, in VA
 #NB : "connected" means the load is connected to the PV, disconnected means it is connected to EDF
 ###############################IMPORTS#####################################
 import time
@@ -31,28 +31,40 @@ MPPT_Enabled = False
 bus = 0
 ############################################################################""
 class c_System_Data :
-	"""Structure all system data"""
+	"""Structure for all system data"""
 	def __init__(self):
-		self.date = 'jjmmaa'
-		self.time = 0
-		self.s_Battery_State = "OK" # "LOW" "HIGH"
-		self.u8_BatteryLevel = 50
-		self.f32_BatteryPower = 0.0
-		self.AllLoadsConnected = False
-		self.AllLoadsDisconnected = True
-		self.f32_MPPT_Power = 0.0
-		self.f32_LSW_Power = 0.0
-		self.LoadStatusTable = ['EDF','EDF','EDF','EDF','EDF']
-		self.LoadSPowerTable = [0.0,0.0,0.0,0.0,0.0]
-		self.MPPT_Enabled = False
-		self.BMS_Enabled = False
-		self.INV_Enabled = False
-		self.LSW_Enabled = False
-
+		self.date = 'jjmmaa' #current real time date (not managed yet)
+		self.time = 0 #current time (currently only a simple counter ++)
+		self.s_Battery_State = "OK" # Battery charge status ("LOW", "OK", "HIGH") Managed with a hysteresis 
+								# to prevent too rapid commutation of the switches)
+		self.u8_BatteryLevel = 70 # battery charge percentage (usable is 50 to 80)
+		self.f32_BatteryPower = 0.0 # battery power in VA, + is out of the battery, - is into the battery
+		self.AllLoadsConnected = False #verification based on LSW module info (load status)
+		self.AllLoadsDisconnected = True #verification based on LSW module info (load status)
+		self.f32_MPPT_Power = 0.0 #MPPT power in VA. 
+		self.f32_LSW_Power = 0.0 #Sum of the power consumed by all the loads connected
+		self.LoadStatusTable = ['EDF','EDF','EDF','EDF','EDF'] #EDF or PV
+		self.LoadSPowerTable = [0.0,0.0,0.0,0.0,0.0] #power for each load (VA)
+		self.MPPT_Enabled = False #module status
+		self.BMS_Enabled = False #module status
+		self.INV_Enabled = False #module status
+		self.LSW_Enabled = False #module status
+		
+#########################################################################
+# b_InitSystem
+# Global PV system initialisation procedure. Checks that other modules
+# are ready (sends Enable and waits for their status message). Test also 
+# the CAN bus communication at the same time.
+# Input Args : SData : system data class
+# Output Args : err   : if initialisation error (one module did not respond)
+# 			 bus   : for the CAN bus lower code layers
+# 			 q     : CAN message queue (buffer)
+#			 SData : system data  class, to return the module states
+#########################################################################
 def b_InitSystem(SData):
     err = False
     PICAN_LED_init()
-    bus,q = CAN_init()
+    bus,q = CAN_init() # IMPORTANT : creation of a thread. a small program will be running in parallel to this one to bufferize the can messages when they arrive. (see CAN_init() in CAN_appli.py)
     
     #init of other modules
     
@@ -71,7 +83,7 @@ def b_InitSystem(SData):
     if not SData.BMS_Enabled :
         print('ERR BMS NOT RESPONDING')
         print('20s timeout exceeded')
-        raise SystemError("Initialisation error") #terminate program
+        return True,0,0,0 #terminate program
     
     for i in range(0,30): #LSW and INV init
         time.sleep(0.4)#wait 0.5s
@@ -99,14 +111,27 @@ def b_InitSystem(SData):
     if not SData.INV_Enabled :
         print('ERR INV NOT RESPONDING')
         print('20s timeout exceeded')
-        raise SystemError("Initialisation error") #terminate program
+        return True,0,0,0 #terminate program
     if not SData.LSW_Enabled :
         print('ERR LSW NOT RESPONDING')
         print('20s timeout exceeded')
-        raise SystemError("Initialisation error") #terminate program
+        return True,0,0,0 #terminate program
     
     return err,bus,q,SData
-
+    
+#########################################################################
+# u8_Load_Choice_Algorithm
+# Algorithm for chosing the best load to fill the power delta, accoring to
+# each load's power consumption (the load's power is measured even if they
+# are connected to the EDF grid). This algo will be called for each load 
+# connection/disconnection.
+# Input Args : ConnectTo : 'PV' or 'EDF'. indicates if we want to resp. connect
+#					  or disconnect a load.
+#			f32_power_batt
+#			LoadStatusTable
+#			LoadSPowerTable
+# Output Args : Matching_load_num : result of the algo. Load to be commuted
+#########################################################################
 def u8_Load_Choice_Algorithm(ConnectTo, f32_power_batt, LoadStatusTable, LoadSPowerTable):
 	#get load power data
         Matching_load_num = 'FF'
@@ -123,7 +148,11 @@ def u8_Load_Choice_Algorithm(ConnectTo, f32_power_batt, LoadStatusTable, LoadSPo
                 #print('no')
                 
         return Matching_load_num
-			
+        
+#########################################################################
+# Connect_Load
+# to connect a load to the PV system.
+#########################################################################		
 def Connect_Load(bus,LoadStatusTable, LoadSPowerTable, f32_BatteryPower):
     #Load choice Algorithm
     LoadNum = u8_Load_Choice_Algorithm('PV',abs(f32_BatteryPower),LoadStatusTable, LoadSPowerTable)
@@ -134,7 +163,11 @@ def Connect_Load(bus,LoadStatusTable, LoadSPowerTable, f32_BatteryPower):
         #send CAN message to LSW to connect optimal load
         print("Connecting load "+str(LoadNum))
         SW_Loads(LoadNum,'PV',bus)
-	
+
+#########################################################################
+# Disconnect_Load
+# to connect a load to the EDF grid.
+#########################################################################	
 def Disconnect_Load(bus,LoadStatusTable, LoadSPowerTable, f32_BatteryPower):
     #Load choice Algorithm
     LoadNum = u8_Load_Choice_Algorithm('EDF',abs(f32_BatteryPower),LoadStatusTable, LoadSPowerTable)
@@ -146,10 +179,14 @@ def Disconnect_Load(bus,LoadStatusTable, LoadSPowerTable, f32_BatteryPower):
         print("Disconnecting load "+str(LoadNum))
         SW_Loads(LoadNum,'EDF',bus)
 
+#########################################################################
+# MainAlgorithm
+# 
+#########################################################################
 def MainAlgorithm():
     SData = c_System_Data()
     bus = 0
-    #Creation of data files
+    #Creation of data files for data logging
     Init_file_batt_csv('battery.csv')
     Init_file_chargetot_csv('charge_tot.csv')
     Init_file_charge_csv('charge1.csv')
@@ -170,11 +207,12 @@ def MainAlgorithm():
             print("Second initialisation error. System shut down...")
             raise SystemError("Initialisation error") #terminate program
     print("Initialisation done.")
+    
     counter = 0
     while(True):
-        time.sleep(0.005)#0.5ms
+        time.sleep(0.005)#in seconds
         counter += 1
-        #check state of watchdogs (check that all modules are sending data on the CAN bus, i.e. the data is fresh and relevant)
+        #TODO : Add watchdogs and check their state (check that all modules are sending data on the CAN bus, i.e. the data is fresh and relevant). If one module stopped sending messages, de-enable it and relauch the initialisation procedure.
         if CAN_msg_queue.empty() != True:	# Check if there is a message in queue
             SData = CAN_RX_Parser(CAN_Receive_msg(bus,CAN_msg_queue),SData) #read CAN message and parse it
                         
@@ -182,8 +220,7 @@ def MainAlgorithm():
             counter = 0
             print('--------------entering main algo------------')
             SData.time += 1
-            Log_Data(SData) #stor data in log files
-            #Main Algorithm
+            Log_Data(SData) #store data in log files
             #test battery level
             if(SData.u8_BatteryLevel > MAX_BATTERY_LEVEL):
                 SData.s_Battery_State = "HIGH"
@@ -191,8 +228,6 @@ def MainAlgorithm():
                 SData.s_Battery_State = "LOW"
             elif(SData.u8_BatteryLevel < OK_BATTERY_LEVEL_H and SData.u8_BatteryLevel > OK_BATTERY_LEVEL_L):
                 SData.s_Battery_State = "OK"
-
-            
 
             #code corresponding to different battery states
             if(SData.s_Battery_State == "HIGH"):
@@ -203,6 +238,7 @@ def MainAlgorithm():
                         Enable_MPPT(False,bus)
                     else:
                         Connect_Load(bus,SData.LoadStatusTable,SData.LoadSPowerTable, SData.f32_BatteryPower)
+                        
             elif(SData.s_Battery_State == "LOW"):
                 #code for battery undercharge
                 if(not SData.MPPT_Enabled):
@@ -235,9 +271,6 @@ def MainAlgorithm():
                             Disconnect_Load(bus,SData.LoadStatusTable,SData.LoadSPowerTable,SData.f32_BatteryPower)
                     else:
                         print("consumption equivalent to production +/-"+str(PRECISION_LOAD_BALANCING)+"VA")
-            else:
-                print("Undeclared battery state detected. System shut down...")
-                raise "Undeclared state error" #terminate program
             
             print("Battery State : "+ SData.s_Battery_State)
             print('LSW power = '+str(SData.f32_LSW_Power))
